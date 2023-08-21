87d277ec3950b25e103b0c2b695363aab854bff1
BanKai117 <142318359+BanKai117@users.noreply.github.com>
2023-08-21 21:42:49 +1000

    Add files via upload

---
+++ st.selectbox/st.selectbox/st.selectbox.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header("st.selectbox")
+
+option = st.selectbox(
+    "What is  your favourite colour?",
+    ("Blue", "Red","Green"))
+
+st.write("Your favourite colour is ", option)
