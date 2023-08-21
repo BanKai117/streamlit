8a0734eb989c6164fde62faad51347f645f6ccd0
BanKai117 <142318359+BanKai117@users.noreply.github.com>
2023-08-21 22:59:59 +1000

    Update streamlit_components.py

--- streamlit_components/streamlit_components/streamlit_components.py
+++ streamlit_components/streamlit_components/streamlit_components.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import ydata_profiling 
-from streamlit_pandas_profiling import st_profile
+from streamlit_pandas_profiling import st_profile_report
 
 st.header("`streamlit_pandas_profiling`")
 
